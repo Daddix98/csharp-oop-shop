2b98ef3142d11aecc12beb866baf342e499551b3
Davide Scriboni <davidescriboni94@gmail.com>
2022-04-12 15:52:54 +0200

    prima parte

--- Csharp Shop/Program.cs
+++ Csharp Shop/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+
+
+//------------ CREO OGGETTO 1 ------------//
+
+using Csharp_Shop;
+Prodotto mioProdotto = new Prodotto("AirPods", "Cuffie wireless", 129.90, "22%");
+mioProdotto.GetGeneraCodiceProdotto();
+mioProdotto.GetCalcolaPrezzoIva();
+mioProdotto.GetNomeEsteso();
+mioProdotto.StampaDati();
+
+//----------- CREO OGETTO 2 ---------//
+
+Prodotto mioProdotto2 = new Prodotto("Beats Studio", "Cuffie con padiglione e con cavo", 250.90, "22%");
+mioProdotto2.GetGeneraCodiceProdotto();
+mioProdotto2.GetCalcolaPrezzoIva();
+mioProdotto2.GetNomeEsteso();
+mioProdotto2.StampaDati();
